236c64648404b561330f43043da0b98d404662f5
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-05 14:49:19 +0900

    :sparkles: Feat: Toggle 기능 추가
    기능 및 스타일 추가

--- custom-component/src/component/Toggle.jsx
+++ custom-component/src/component/Toggle.jsx
@@ -1,7 +1,75 @@
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
+import Header from "./common/Header"
 
 const Toggle = () => {
-  return <div>Toggle Component</div>
+  const [toggle, setToggle] = useState(false)
+
+  const handleClick = () => setToggle((prev) => !prev)
+
+  return (
+    <>
+      <Header title="Toggle" />
+      <Container>
+        <Wrap>
+          <input type="checkbox" id="button" />
+          <Circle htmlFor="button" onClick={handleClick} />
+          <Background />
+        </Wrap>
+        <p>Toggle Switch {toggle ? "ON" : "OFF"}</p>
+      </Container>
+    </>
+  )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`
+
+const Wrap = styled.div`
+  width: 100px;
+  background-color: gray;
+  border-radius: 25px;
+  padding: 5px;
+  position: relative;
+  overflow: hidden;
+  input[type="checkbox"] {
+    display: none;
+  }
+  input[type="checkbox"]:checked ~ label {
+    transform: translateX(60px);
+  }
+  input[type="checkbox"]:checked ~ div {
+    left: 0;
+  }
+`
+
+const Circle = styled.label`
+  display: block;
+  width: 40px;
+  height: 40px;
+  position: relative;
+  background-color: white;
+  border-radius: 50%;
+  cursor: pointer;
+  transform: translateX(0);
+  transition: all 0.5s linear;
+  z-index: 1;
+`
+
+const Background = styled.div`
+  width: 120px;
+  height: 50px;
+  background-color: purple;
+  position: absolute;
+  top: 0;
+  left: -120px;
+  transition: all 0.5s linear;
+`
 
 export default Toggle
