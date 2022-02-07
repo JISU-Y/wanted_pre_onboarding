2e84b6ff51dbcb32c27ac2753d7ad05e9cd5cfe7
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-07 16:59:26 +0900

    :bulb: Comment: Tag 컴포넌트 주석 추가
    변수 및 함수 주석 설명 추가

--- custom-component/src/component/Tag.jsx
+++ custom-component/src/component/Tag.jsx
@@ -4,38 +4,43 @@ import Header from "./common/Header"
 import { nanoid } from "nanoid"
 
 const Tag = () => {
+  // tag list의 state
   const [tags, setTags] = useState(null)
+  // input text state
   const [text, setText] = useState("")
 
+  // enter 시 submit 동작
   const handleSubmit = (e) => {
     e.preventDefault()
+    // input의 text를 tags 요소 중 value로 설정, 그의 id는 nanoid 생성
+    // tags가 아무것도 없을 때는 하나만 생성 (prev가 non iterable 하므로)
     setTags((prev) =>
       tags
         ? [{ value: e.target[0].value, id: nanoid() }, ...prev] //
         : [{ value: e.target[0].value, id: nanoid() }]
     )
+    // input 초기화
     setText("")
   }
 
+  // tag 삭제 (id이용 삭제)
   const deleteTag = (id) => setTags((prev) => prev.filter((el) => el.id !== id))
 
   return (
-    <>
+    <Container>
       <Header title="Tag" />
-      <Container>
-        <InputContainer onSubmit={handleSubmit}>
-          <input type="text" placeholder="Press enter to add tags" value={text} onChange={(e) => setText(e.target.value)} />
-          {tags?.map((tag) => (
-            <TagContent key={tag.id}>
-              {tag.value}
-              <button type="button" onClick={() => deleteTag(tag.id)}>
-                x
-              </button>
-            </TagContent>
-          ))}
-        </InputContainer>
-      </Container>
-    </>
+      <InputContainer onSubmit={handleSubmit}>
+        <input type="text" placeholder="Press enter to add tags" value={text} onChange={(e) => setText(e.target.value)} />
+        {tags?.map((tag) => (
+          <TagContent key={tag.id}>
+            {tag.value}
+            <button type="button" onClick={() => deleteTag(tag.id)}>
+              x
+            </button>
+          </TagContent>
+        ))}
+      </InputContainer>
+    </Container>
   )
 }
 
@@ -45,7 +50,11 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   width: 100vw;
-  height: 100vh;
+  position: relative;
+  border: 2px solid gray;
+  border-radius: 5px;
+  margin: 2px 0;
+  padding: 10px 20px;
 `
 
 const InputContainer = styled.form`
