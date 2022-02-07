41569d7a6fc76abbb2e5087d07ca3f48ac66e47e
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-07 16:51:22 +0900

    :bulb: Comment: AutoComplete 컴포넌트 주석 추가
    변수 및 함수에 주석 설명 추가

--- custom-component/src/component/AutoComplete.jsx
+++ custom-component/src/component/AutoComplete.jsx
@@ -2,34 +2,39 @@ import React, { useState } from "react"
 import styled from "styled-components"
 import Header from "./common/Header"
 
+// database
 const data = ["antique", "vintage", "중고A급", "rustic", "refurbished"]
 
 const AutoComplete = () => {
+  // filter 후 결과가 담길 state
   const [results, setResults] = useState([])
+  // input text state
   const [text, setText] = useState("")
 
+  // 단어 찾기
   const searchWord = (word) => {
+    // input value set
     setText(word)
+    // 단어가 빈 문자열 일 시 results initial state로 변경 후 filtering 동작하지 못하도록 early return
     if (word === "") {
       setResults([])
       return
     }
+    // 단어 포함 여부 filter (대소문자 구분없이)
     setResults(data.filter((el) => el.includes(word) || el.includes(word.toUpperCase())))
   }
 
   return (
-    <>
+    <Container>
       <Header title="AutoComplete" />
-      <Container>
-        <SearchContainer noResults={!results.length}>
-          <input type="text" onChange={(e) => searchWord(e.target.value)} value={text} />
-          <button onClick={() => setText("")}>✖</button>
-          {results.map((result) => (
-            <Result key={result}>{result}</Result>
-          ))}
-        </SearchContainer>
-      </Container>
-    </>
+      <SearchContainer noResults={!results.length}>
+        <input type="text" onChange={(e) => searchWord(e.target.value)} value={text} />
+        <button onClick={() => setText("")}>✖</button>
+        {results.map((result) => (
+          <Result key={result}>{result}</Result>
+        ))}
+      </SearchContainer>
+    </Container>
   )
 }
 
@@ -39,7 +44,11 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   width: 100vw;
-  height: 100vh;
+  position: relative;
+  border: 2px solid gray;
+  border-radius: 5px;
+  margin: 2px 0;
+  padding: 10px 20px;
 `
 
 const SearchContainer = styled.div`
@@ -55,6 +64,7 @@ const SearchContainer = styled.div`
     padding: 8px 10px;
     font-size: 18px;
     border: none;
+    /* 결과가 아무것도 없을 때 input의 border 삭제 */
     border-bottom: ${(props) => (props.noResults ? "none" : "1px solid #c4c4c4")};
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
