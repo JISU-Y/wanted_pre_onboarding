79d47ac19ab261ffb720c2f361d46dea37031f99
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-08 00:09:41 +0900

    :recycle: Refactor: Toggle label 수정
    Label after before 삭제, Switch Rail div 생성

--- custom-component/src/component/Toggle.jsx
+++ custom-component/src/component/Toggle.jsx
@@ -8,9 +8,11 @@ const Toggle = () => (
     <Wrap>
       {/* checkbox input (보이지 않음) */}
       <input type="checkbox" id="button" />
-      {/* 실제로 input check를 대신하는 보여지는 label
-      Switch rail 모양, 흰색 버튼 모양 나타냄 */}
-      <Switch htmlFor="button" />
+      {/* Switch rail 모양 */}
+      <SwitchRail>
+        {/* 실제로 input check를 대신하는 보여지는 label 흰색 버튼 모양 나타냄 */}
+        <Switch htmlFor="button" />
+      </SwitchRail>
       {/* Toggle Switch ON/OFF 나타냄 */}
       <p></p>
     </Wrap>
@@ -30,9 +32,8 @@ const Container = styled.div`
   padding: 10px 20px;
 `
 
-// switch container (버튼 및 버튼 rail 모양)
-const Switch = styled.label`
-  display: block;
+// switch container (버튼 rail 모양)
+const SwitchRail = styled.div`
   width: 100px;
   height: 50px;
   border-radius: 25px;
@@ -41,32 +42,32 @@ const Switch = styled.label`
   transform: translateX(0);
   background-color: gray;
   overflow: hidden;
-  // 흰색 동그라미 버튼
-  &::after {
-    content: "";
-    width: 40px;
-    height: 40px;
-    position: absolute;
-    top: 5px;
-    left: 5px;
-    background-color: white;
-    border-radius: 50%;
-    cursor: pointer;
-    transition: all 0.5s linear;
-    z-index: 1;
-  }
   /* 배경 */
   &::before {
     content: "";
     position: absolute;
     top: 0px;
-    left: 0px;
+    left: -100%;
     width: 100%;
     height: 100%;
-    transform: translateX(-100%);
     background-color: purple;
     transition: all 0.5s linear;
   }
+`
+
+// 흰색 동그라미 버튼
+const Switch = styled.label`
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  background-color: white;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: all 0.5s linear;
+  z-index: 1;
+  display: block;
 `
 
 // 버튼과 글씨
@@ -79,16 +80,17 @@ const Wrap = styled.div`
   input[type="checkbox"] {
     display: none;
   }
-  /* click 되었을 때(checkbox가 check 되었을 때) Switch */
-  input[type="checkbox"]:checked ~ label {
-    /* switch에서 동그란 버튼 위치 오른쪽으로 옮김 */
-    &::after {
-      left: 55px;
-    }
+  /* Switch Rail 배경 transition */
+  input[type="checkbox"]:checked ~ div {
     /* 배경도 오른쪽으로 옮김 */
     &::before {
-      transform: translateX(0);
+      left: 0;
     }
+  }
+  /* click 되었을 때(checkbox가 check 되었을 때) Switch */
+  input[type="checkbox"]:checked ~ div > label {
+    /* switch에서 동그란 버튼 위치 오른쪽으로 옮김 */
+    left: 55px;
   }
   /* Toggle Switch 글 default OFF */
   p::after {
